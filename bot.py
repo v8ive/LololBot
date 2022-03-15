822ad1b0847efc201a92bbaac73442ebd36b11af
v8ive <v8ive369@gmail.com>
2022-03-15 01:20:59 -0400

    Update

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ token = config('token')
 
 @bot.event
 async def on_ready():
+    db.execute(f"create table if not exists users(id bigint not null primary key, name varchar(100) not null)")
+    mydb.commit()
     guilds = ", ".join(bot.guilds) if len(bot.guilds) > 1 else bot.guilds[0]
-    print(f"Connected to : {guilds}")
-    print(f"Logged in as : {bot.user}")
     users = []
     bots = []
     for user in bot.users:        
@@ -25,11 +25,21 @@ async def on_ready():
                 mydb.commit()
             users.append(user.name)
             
-    print(f"User Count : {len(users)}")
     users = ", ".join(users)
     bots = ", ".join(bots) if len(bots) > 1 else bots[0]
-    print(f"Bots : {bots}")
-    print(f"Users : {users}")
+    
+    status = f'''
+Discord.py Version : {discord.__version__}
+
+Successfully Connected...
+
+Connected to ({bot.user}): {guilds}
+
+Bots  ({len(bots)}): {bots}
+Users ({len(users)}): {users}
+
+'''
+    print(status)
 
 @bot.event
 async def on_member_join(ctx, member:discord.Member):
@@ -37,11 +47,12 @@ async def on_member_join(ctx, member:discord.Member):
     
     embed = discord.Embed(title=f"Welcome {member.name}", color=0xd4af37, description=f"{member.name} has joined the Lolol Crew! :tada: Welcome!")
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text="Lolol Official", icon_url="https://raw.githubusercontent.com/v8ive/LuniBot/main/pics/LuniLogo.png")
+    embed.set_footer(text="Lolol Official", icon_url="https://raw.githubusercontent.com/v8ive/LololBot/master/img/lolol_pfp.png")
+    
+    db.execute(f"insert into users values({member.id}, '{member.name}')")
+    mydb.commit()
             
-    
-    
-    
+    await welcome_chan.send(embed=embed)
 
 if __name__ == "__main__":
     cogs = []
